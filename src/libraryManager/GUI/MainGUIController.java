354fe9515c7085ed50952580c1421d4d2a9b2611
Sa'Kagé <aml-os@gmx.de>
2018-04-06 17:10:05 +0200

    Added controller for the main GUI

---
+++ src/libraryManager/GUI/MainGUIController.java
@@ -0,0 +1,16 @@
+package libraryManager.GUI;
+
+import javafx.fxml.FXML;
+
+/**
+ * 
+ * @author Alexander Luebbe
+ *
+ */
+public class MainGUIController {
+
+	@FXML
+	private void showAddBookDialog() {
+		System.out.println("Adding book");
+	}
+}
