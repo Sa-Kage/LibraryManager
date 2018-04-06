fec7b67bb6ea5816567e842fd7785e9faf02ec1e
Sa'Kagé <aml-os@gmx.de>
2018-04-06 19:23:55 +0200

    Dialog for adding books now shows up

--- src/libraryManager/Main.java
+++ src/libraryManager/Main.java
@@ -1,29 +1,40 @@
 package libraryManager;
 	
+import java.io.IOException;
 import java.util.ResourceBundle;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
-import libraryManager.GUI.MainGUIController;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
+import libraryManager.GUI.AddBookController;
+import libraryManager.GUI.MainGUIController;
 
 public class Main extends Application {
+	
+	private ResourceBundle bundle;
+	private Stage primaryStage;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			ResourceBundle bundle = ResourceBundle.getBundle("libraryManager.resources.language");
+			this.primaryStage = primaryStage;
+			bundle = ResourceBundle.getBundle("libraryManager.resources.language");
+			
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("GUI/MainGUI.fxml"), bundle);
 			BorderPane root = loader.load();
 			MainGUIController mainGUICtrl = loader.getController();
+			
 			mainGUICtrl.initialize(this);
 			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
 			primaryStage.setMaximized(true);
 			primaryStage.show();
 		} catch(Exception e) {
+			// TODO:
 			e.printStackTrace();
 		}
 	}
@@ -36,7 +47,23 @@ public class Main extends Application {
 	 * 
 	 */
 	public void showAddBookDialog() {
-		// TODO Auto-generated method stub
-		
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("GUI/AddBook.fxml"), bundle);
+			BorderPane addBook;
+			addBook = loader.load();
+			AddBookController addBookCtrl = loader.getController();
+			
+			//addBookCtrl.initialize(this);
+			Scene scene = new Scene(addBook);
+			Stage stage = new Stage();
+			stage.setResizable(false);
+			stage.initOwner(primaryStage);
+			stage.initModality(Modality.APPLICATION_MODAL);
+			stage.setScene(scene);
+			stage.showAndWait();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
